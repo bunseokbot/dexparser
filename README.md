1ee4edb8e6375fe7423a2a3b4a094dd4e34e427a
김남준 <admin@smishing.kr>
2015-08-20 15:27:41 +0900

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # dexparser
 DEX Parser for Python
+
---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+		name = 'dexparser',
+		version = '0.0.1',
+		py_modules = ['dexparser', 'disassembler'],
+		author = 'bunseokbot',
+		author_email = 'admin@smishing.kr',
+		url = 'https://github.com/bunseokbot/dexparser',
+		description = 'DEX Parser for Android Analyst'
+	 )
