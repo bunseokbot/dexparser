4e9092b998864631320ecc5c5f03fc70cf10b41a
Namjun Kim <bunseokbot@gmail.com>
2019-12-24 23:32:38 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,26 @@
-from distutils.core import setup
+from setuptools import setup
+
+with open("README.md") as f:
+	long_description = f.read() 
 
 setup(
 	name = 'dexparser',
-	version = '1.0.0',
+	version = '1.0.1',
 	py_modules = ['dexparser'],
 	author = 'austinkim',
 	author_email = 'bunseokbot@gmail.com',
 	url = 'https://github.com/bunseokbot/dexparser',
-	description = 'DEX Parser for Android Analyst'
+	description = 'Powerful DEX file format parser for Pythonist',
+	long_description = long_description,
+    long_description_content_type = 'text/markdown',
+	classifiers = [
+		'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+		'Programming Language :: Python :: 3.8',
+		"License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+	python_requires='>=3.5',
 )
