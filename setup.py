0e9ad14030d8f7b140a426ad496bd0fb9e442542
Namjun Kim <bunseokbot@gmail.com>
2021-02-11 14:47:50 +0900

    Update module version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md") as f:
 
 setup(
 	name = 'dexparser',
-	version = '1.0.2',
+	version = '1.0.3',
 	py_modules = ['dexparser'],
 	author = 'austinkim',
 	author_email = 'austin.njkim@gmail.com',
